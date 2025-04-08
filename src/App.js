42bec62ee21ebf64112f6fa2155e67d6be9eb47b
Vaibhav Kumar <vaibhav.kumar@qudasoft.com>
2025-04-08 16:17:21 +0530

    integrate api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { useEffect } from "react";
 import "./App.css";
+import { get } from "aws-amplify/api";
 
 function App() {
-    return <div className="App">App</div>;
+  useEffect(() => {
+      const fetchApi = async () => {
+          try {
+              const restOperation = get({
+                  apiName: "itemsApi",
+                  path: "/items",
+              });
+              const response = await restOperation.response;
+              console.log("GET call succeeded: ", response);
+          } catch (e) {
+              console.log("GET call failed: ", JSON.parse(e.response.body));
+          }
+      };
+      fetchApi();
+  }, []);
+
+  return <div className="App">App</div>;
 }
 
 export default App;
